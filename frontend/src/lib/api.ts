71b7a9038beac6911645e1dd0d6765d89f1623cf
aristides30 <aristidesliros@gmail.com>
2026-02-05 18:23:25 -0300

    fix_api_url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = typeof window !== 'undefined' ? (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000') : 'http://backend:8000';
+if (typeof window !== 'undefined') {
+  console.log('API Base URL:', baseURL);
+}
 const api = axios.create({
-  baseURL: typeof window !== 'undefined' ? (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000') : 'http://backend:8000',
+  baseURL,
 });
 
 export default api;
